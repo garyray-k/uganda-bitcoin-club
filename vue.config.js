13605404cd112f8949c37cf352f2c85f86504a9a
Gary Krause <gary@codechief.dev>
2022-05-23 06:45:58 -0400

    Placeholder text. QR Code on Dontate. Setup for actual URL.

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@
 const path = require("path");
 
 module.exports = {
-  publicPath: "/uganda-bitcoin-club",
+  publicPath: "/",
   chainWebpack: (config) => {
     config.module
       .rule('images')
