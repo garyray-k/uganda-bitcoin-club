fdbd2398163ca35d32b14d785f33624fdbe61b7a
Gary Krause <gary@codechief.dev>
2022-05-26 20:13:39 -0400

    About content update.

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,7 +1,27 @@
 <template>
   <div class="about">
     <h1>About</h1>
-    <h3>Coming soon...</h3>
+    <p>
+      Have you heard about Bitcoin recently?
+      <br />
+      <br />
+      Do you want to know how it is different from and superior to the rest of
+      the other cryptocurrencies?
+      <br />
+      <br />
+      Already deep in the rabbit hole, enjoying your discovery of this
+      revolutionary technology?
+      <br />
+      <br />
+      Whatever level of knowledge or interest you have in Bitcoin, you are
+      welcome to join us.
+      <br />
+      <br />
+      Come meet fellow Bitcoiners in Uganda — share, learn, and have fun!
+      <br />
+      <br />
+      See you soon.
+    </p>
   </div>
 </template>
 <script lang="ts">
@@ -28,5 +48,15 @@ li {
 
 a {
   color: #42b983;
+}
+
+p {
+  word-break: break-word;
+  overflow-wrap: break-word;
+  padding: 20px;
+  align-self: center;
+  align-content: center;
+  max-width: 750px;
+  margin: auto;
 }
 </style>
